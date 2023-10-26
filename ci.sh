a6a02357e3766dd7d0cb518d2e84a5ee44c30f32
Jason Hsu <jhsu802701@jasonhsu.com>
2023-10-26 15:08:47 -0500

    Added container_create to ci.sh

--- ci.sh
+++ ci.sh
@@ -3,5 +3,10 @@
 DISTRO='debian'
 DOCKER_IMAGE="ghcr.io/$OWNER/docker-$DISTRO-$SUITE-$ABBREV"
 CONTAINER="container-$DISTRO-$SUITE-$ABBREV"
+WORK_DIR="tmp/$ABBREV/$SUITE"
 
 bash setup.sh $ABBREV $SUITE $DOCKER_IMAGE $CONTAINER
+
+cd $WORK_DIR
+
+bash container_create.sh $DOCKER_IMAGE $CONTAINER
