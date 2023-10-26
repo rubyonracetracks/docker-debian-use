9256e3483e3c1ffd12babf8c95eb8672ab2851fc
Jason Hsu <jhsu802701@jasonhsu.com>
2023-10-26 18:23:55 -0500

    Run /usr/local/bin/check in Docker container in CI

--- ci.sh
+++ ci.sh
@@ -6,7 +6,10 @@ CONTAINER="container-$DISTRO-$SUITE-$ABBREV"
 WORK_DIR="tmp/$ABBREV/$SUITE"
 
 bash setup.sh $ABBREV $SUITE $DOCKER_IMAGE $CONTAINER
-
+wait
 cd $WORK_DIR
-
+wait
 bash container_create.sh $DOCKER_IMAGE $CONTAINER
+wait
+docker exec -it $CONTAINER '/usr/local/bin/check'
+wait
